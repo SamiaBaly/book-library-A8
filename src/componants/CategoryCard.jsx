44349c51598072861a0e331764d84bdc6a0f0090
SamiaBaly <samiabaly116@gmail.com>
2026-04-30 20:03:42 +0600

    add category json

--- src/componants/CategoryCard.jsx
+++ src/componants/CategoryCard.jsx
@@ -1,15 +1,9 @@
-
 import React from 'react';
 
-
-const CategoryCard = ({book}) => {
+const CategoryCard = () => {
   return (
-    <div
-      className={`border px-2 py-4 rounded-xl mx-1 text-center cursor-pointer`}
-    >
-      <div>
-        <h2 className="font-bold text-xl text-[#6c5ce7]">{book.category}</h2>
-      </div>
+    <div>
+      
     </div>
   );
 };
