e517a15515f02720a102d917fba4bca5605b18fd
SamiaBaly <samiabaly116@gmail.com>
2026-05-04 23:57:49 +0600

    fixed error 2

--- src/componants/NewBookName.jsx
+++ src/componants/NewBookName.jsx
@@ -1,10 +1,11 @@
 import { Button } from '@heroui/react';
 import React from 'react';
 import Marquee from 'react-fast-marquee';
-import data from "../../public/data.json"
+
 
 export const getbooks=async()=>{
-  return data;
+  const res = await fetch('https://book-library-a8-56mz.vercel.app/data.json');
+  return res.json();
 }
 
 const NewBookName =async () => {
